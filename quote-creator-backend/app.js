0f84f4d38cef3397b4afb20e9c16693fe5c47fad
abhishek.mathew <abhishek.mathew@focaloid.com>
2022-03-21 03:16:00 +0530

    initial commit

---
+++ quote-creator-backend/app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const router = express.Router();
+const textToImage = require("text-to-image");
+
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+router.post("/api/download", async (req, res) => {
+  await textToImage.generate(req.body.quote, {
+    debug: true,
+    debugFilename: "./downloads/quote.png",
+    maxWidth: 1000,
+    customHeight: 500,
+    fontSize: 75,
+    lineHeight: 100,
+    fontFamily: "Lato",
+    fontPath: "./Lato-BoldItalic.ttf",
+    bgColor: "black",
+    textColor: req.body.colorString,
+    textAlign: "center",
+    verticalAlign: "center",
+  });
+  res.download("./downloads/quote.png");
+});
+
+app.use("/", router);
+
+app.listen(4000, () => console.log("SERVER STARTED"));
