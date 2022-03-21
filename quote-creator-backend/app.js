93f6847d0224e2b792d26b46b1adfd858f372c86
abhishek.mathew <abhishek.mathew@focaloid.com>
2022-03-21 21:41:18 +0530

    debug fixes

--- quote-creator-backend/app.js
+++ quote-creator-backend/app.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 const router = express.Router();
 const textToImage = require("text-to-image");
 
-require('dotenv').config();
+require("dotenv").config();
 
 app.use(cors());
 
@@ -13,21 +13,26 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 router.post("/download", async (req, res) => {
-  await textToImage.generate(req.body.quote, {
-    debug: true,
-    debugFilename: "./downloads/quote.png",
-    maxWidth: 1000,
-    customHeight: 500,
-    fontSize: 75,
-    lineHeight: 100,
-    fontFamily: "Lato",
-    fontPath: "./Lato-BoldItalic.ttf",
-    bgColor: "black",
-    textColor: req.body.colorString,
-    textAlign: "center",
-    verticalAlign: "center",
-  });
-  res.download("./downloads/quote.png");
+  try {
+    console.log("API call hit!");
+    await textToImage.generate(req.body.quote, {
+      debug: true,
+      debugFilename: "./downloads/quote.png",
+      maxWidth: 1000,
+      customHeight: 500,
+      fontSize: 75,
+      lineHeight: 100,
+      fontFamily: "Lato",
+      fontPath: "./Lato-BoldItalic.ttf",
+      bgColor: "black",
+      textColor: req.body.colorString,
+      textAlign: "center",
+      verticalAlign: "center",
+    });
+    res.download("./downloads/quote.png");
+  } catch (error) {
+    console.error(error);
+  }
 });
 
 app.use("/api", router);
